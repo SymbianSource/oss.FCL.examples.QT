179c150aa0ebf3225de7ca98549887dbffcc8f8f
Wesley Thierry <wesleyt@symbian.org>
2010-10-20 12:55:41 -0700

    Adding mixedview project

---
+++ MixedView/main.cpp
@@ -0,0 +1,39 @@
+#include <QtGui/QApplication>
+#include "mainwindow.h"
+
+// Needed Symbian specific headers
+#ifdef Q_OS_SYMBIAN
+#include <eikenv.h>
+#include <eikappui.h>
+#include <aknenv.h>
+#include <aknappui.h>
+#include <coeaui.h>
+#endif
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+
+
+
+
+    MainWindow w;
+    a.installEventFilter(&w);
+    w.setSizePolicy(QSizePolicy::Ignored, QSizePolicy::Ignored);
+
+    // Symbian specific code
+#ifdef Q_OS_SYMBIAN
+    CAknAppUi* appUi = dynamic_cast<CAknAppUi*> (CEikonEnv::Static()->AppUi());
+    if(appUi){
+      TRAP_IGNORE( appUi->SetOrientationL( CAknAppUi::EAppUiOrientationPortrait ) );
+    }
+#endif
+
+#if defined(Q_WS_S60)
+    w.showMaximized();
+#else
+    w.show();
+#endif
+
+    return a.exec();
+}
---
+++ MixedView/mainwindow.h
@@ -0,0 +1,59 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QPushButton>
+#include <QGraphicsEllipseItem>
+#include <QTimer>
+#include <QAccelerometer>
+#include <QLineEdit>
+#include <QGraphicsProxyWidget>
+#include <QMediaPlayer>
+#include <QMediaPlaylist>
+#include <QMediaServiceProvider>
+#include <QAudioOutput>
+#include <QFile>
+QTM_USE_NAMESPACE
+
+namespace Ui {
+    class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+private:
+    Ui::MainWindow *ui;
+    QGraphicsEllipseItem *mPacman;
+    QTimer *mUpdateTimer;
+    int mPacState;
+    bool bIsMoving;
+    QAccelerometer *mAccelerometer;
+    qreal xAxis, yAxis;
+    QPointF mLastPt;
+    bool mPause;
+    QPushButton *pauseButton;
+    QMediaPlayer *mPlayer;
+    QMediaPlaylist *mPlayList;
+    QGraphicsScene *mScene;
+    int mNumHits;
+    QAudioOutput* mAudio;
+    QFile file;
+
+private slots:
+    void updateGraphics();
+    void updateReading();
+    void pause();
+    void statusChanged(QMediaPlayer::MediaStatus);
+
+private:
+    void setupBoundries();
+
+};
+
+#endif // MAINWINDOW_H
